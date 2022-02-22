54e145c6c4ce67d00bb0853546b88330114b6ebc
Georgii Budanov <GGBV@simcorp.com>
2022-02-22 09:12:50 +0100

    adding gitignore file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Introduction_to_GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
